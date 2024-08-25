d93d47e0a7fc8a33462ede7f1092232c6144cb16
seyamasan <kst_apr_24@icloud.com>
2024-08-25 17:56:41 +0900

    いらないimport削除

--- app/src/main/java/com/example/mentalrecordapplication/record_mood/viewmodel/RecordMoodActivityViewModel.kt
+++ app/src/main/java/com/example/mentalrecordapplication/record_mood/viewmodel/RecordMoodActivityViewModel.kt
@@ -1,7 +1,6 @@
 package com.example.mentalrecordapplication.record_mood.viewmodel
 
 import android.app.Application
-import android.view.MenuItem
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
