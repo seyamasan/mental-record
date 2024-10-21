0efa6d0890097a2b5544e68107fe3c000056daee
seyamasan <kst_apr_24@icloud.com>
2024-10-21 20:35:27 +0900

    ビルドバージョンとテキストの色変えた

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "jp.example.mentalrecordapplication"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
--- app/src/main/java/jp/example/mentalrecordapplication/main/view/MainActivity.kt
+++ app/src/main/java/jp/example/mentalrecordapplication/main/view/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -58,12 +59,14 @@ fun MainView(modifier: Modifier = Modifier) {
         ) {
             Text(
                 text = stringResource(id = R.string.mental_name),
-                fontSize = TextUtil.dpToSp(dp = 32.dp)
+                fontSize = TextUtil.dpToSp(dp = 32.dp),
+                color = Color.White
             )
 
             Text(
                 text = stringResource(id = R.string.record_name),
-                fontSize = TextUtil.dpToSp(dp = 32.dp)
+                fontSize = TextUtil.dpToSp(dp = 32.dp),
+                color = Color.White
             )
         }
     }
