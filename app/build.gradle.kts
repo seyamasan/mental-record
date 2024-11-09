24209d0c3a5932ac2dbb1b739e02e81831df9796
seyamasan <kst_apr_24@icloud.com>
2024-11-09 13:46:45 +0900

    バージョンアップ忘れてた

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "jp.example.mentalrecordapplication"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
