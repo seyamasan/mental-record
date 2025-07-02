637cb3eb34be9a9853b2b5bd67575cb56bd67a32
seyamasan <kst_apr_24@icloud.com>
2025-07-02 20:26:27 +0900

    feat: Raised api level.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "jp.example.mentalrecordapplication"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "jp.example.mentalrecordapplication"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.1"
 
