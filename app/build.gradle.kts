d2ab5f8f7a796fd57105be85d776ad1b1070cab3
seyamasan <kst_apr_24@icloud.com>
2025-07-15 20:45:56 +0900

    test: Added tests for DataSource.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,6 +95,7 @@ dependencies {
     testImplementation(libs.junit)
     testImplementation(libs.truth)
     testImplementation(libs.mockk)
+    testImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
---
+++ app/src/test/java/jp/example/mentalrecordapplication/data/local/MoodDataSourceImplTest.kt
@@ -0,0 +1,127 @@
+package jp.example.mentalrecordapplication.data.local
+
+import com.google.common.truth.Truth.assertThat
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import jp.example.mentalrecordapplication.data.local.room.MoodDao
+import jp.example.mentalrecordapplication.data.local.room.MoodEntity
+import jp.example.mentalrecordapplication.utils.types.DefaultMoodType
+import jp.example.mentalrecordapplication.utils.types.TimeOfDayType
+import kotlinx.coroutines.test.runTest
+import org.junit.BeforeClass
+import org.junit.Test
+
+class MoodDataSourceImplTest {
+
+    companion object {
+        private lateinit var dao: MoodDao
+        private lateinit var dataSource: MoodDataSourceImpl
+
+        private var dummyEntity: MoodEntity = MoodEntity(
+            id = 0,
+            mood = DefaultMoodType.HAPPY,
+            date = "2024/11/11",
+            timeOfDay = TimeOfDayType.MORNING,
+            memo = "memo"
+        )
+
+        @JvmStatic
+        @BeforeClass
+        fun setup() {
+            dao = mockk(relaxed = true)
+            dataSource = MoodDataSourceImpl(dao)
+        }
+    }
+
+    @Test
+    fun insert_succeeds() = runTest {
+        // Given
+        coEvery { dao.insert(dummyEntity) } returns Unit
+
+        // When
+        val result = dataSource.insert(dummyEntity)
+
+        // Then
+        assertThat(result).isTrue()
+        coVerify { dao.insert(dummyEntity) }
+    }
+
+    @Test
+    fun insert_should_fail_for_unknown_reason() = runTest {
+        // Given
+        coEvery { dao.insert(dummyEntity) } throws Exception("DB error")
+
+        // When
+        val result = dataSource.insert(dummyEntity)
+
+        // Then
+        assertThat(result).isFalse()
+        coVerify { dao.insert(dummyEntity) }
+    }
+
+    @Test
+    fun deleteById_succeeds() = runTest {
+        // Given
+        coEvery { dao.deleteById(1) } returns Unit
+
+        // When
+        val result = dataSource.deleteById(1)
+
+        // Then
+        assertThat(result).isTrue()
+        coVerify { dao.deleteById(1) }
+    }
+
+    @Test
+    fun deleteById_should_fail_for_unknown_reason() = runTest {
+        // Given
+        coEvery { dao.deleteById(1) } throws Exception("DB error")
+
+        // When
+        val result = dataSource.deleteById(1)
+
+        // Then
+        assertThat(result).isFalse()
+        coVerify { dao.deleteById(1) }
+    }
+
+    @Test
+    fun selectAll_succeeds() = runTest {
+        // Given
+        coEvery { dao.selectAll() } returns listOf(dummyEntity)
+
+        // When
+        val result = dataSource.selectAll()
+
+        // Then
+        assertThat(result).isNotEmpty()
+        coVerify { dao.selectAll() }
+    }
+
+    @Test
+    fun deleteAll_succeeds() = runTest {
+        // Given
+        coEvery { dao.deleteAll() } returns Unit
+
+        // When
+        val result = dataSource.deleteAll()
+
+        // Then
+        assertThat(result).isTrue()
+        coVerify { dao.deleteAll() }
+    }
+
+    @Test
+    fun deleteAll_should_fail_for_unknown_reason() = runTest {
+        // Given
+        coEvery { dao.deleteAll() } throws Exception("DB error")
+
+        // When
+        val result = dataSource.deleteAll()
+
+        // Then
+        assertThat(result).isFalse()
+        coVerify { dao.deleteAll() }
+    }
+}
