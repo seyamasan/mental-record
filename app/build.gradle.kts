6e548c44e55acd60dec7aece66d0fadd6cfa431a
seyamasan <kst_apr_24@icloud.com>
2025-07-15 19:46:09 +0900

    chore: Added truth.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,6 +93,7 @@ dependencies {
 
     // test
     testImplementation(libs.junit)
+    testImplementation(libs.truth)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
