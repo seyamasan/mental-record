86578150b17100b0d3134f6962f2f9684c1aedf4
seyamasan <kst_apr_24@icloud.com>
2025-08-22 20:08:05 +0900

    feat: Implemented stringToLocalDate in DateUtil

--- app/src/main/java/jp/example/mentalrecordapplication/data/local/room/MoodEntity.kt
+++ app/src/main/java/jp/example/mentalrecordapplication/data/local/room/MoodEntity.kt
@@ -5,10 +5,10 @@ import androidx.annotation.RequiresApi
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import jp.example.mentalrecordapplication.utils.DateUtil
 import jp.example.mentalrecordapplication.utils.types.DefaultMoodType
 import jp.example.mentalrecordapplication.utils.types.TimeOfDayType
 import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 
 @Entity (tableName = "mood_table")
 data class MoodEntity (
@@ -22,8 +22,6 @@ data class MoodEntity (
     val localDate: LocalDate
         @RequiresApi(Build.VERSION_CODES.O)
         get() {
-            // 日付フォーマットを確認し、適切に修正
-            val formatter = DateTimeFormatter.ofPattern("yyyy/M/d")
-            return LocalDate.parse(date, formatter)
+            return DateUtil.stringToLocalDate(date)
         }
 }
--- app/src/main/java/jp/example/mentalrecordapplication/utils/DateUtil.kt
+++ app/src/main/java/jp/example/mentalrecordapplication/utils/DateUtil.kt
@@ -1,6 +1,10 @@
 package jp.example.mentalrecordapplication.utils
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.Date
 import java.util.Locale
 
@@ -9,5 +13,11 @@ object DateUtil {
         // yyyy/MM/ddのフォーマットに変換
         val formatter = SimpleDateFormat("yyyy/M/dd", Locale.getDefault())
         return formatter.format(Date(millis))
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun stringToLocalDate(date: String): LocalDate {
+        val formatter = DateTimeFormatter.ofPattern("yyyy/M/d")
+        return LocalDate.parse(date, formatter)
     }
 }
