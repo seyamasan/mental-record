e297c0bdf29eb7ff3f64733bd0405c15e83f5240
seyamasan <kst_apr_24@icloud.com>
2025-08-31 14:35:54 +0900

    feat: Implemented EmptyFilterRecordView

---
+++ app/src/main/java/jp/example/mentalrecordapplication/ui/common/button/MediumIconElevatedButton.kt
@@ -0,0 +1,63 @@
+package jp.example.mentalrecordapplication.ui.common.button
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import jp.example.mentalrecordapplication.ui.theme.MentalRecordAppTheme
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@Composable
+fun MediumIconElevatedButton(
+    icon: ImageVector,
+    text: String,
+    onTap: () -> Unit
+) {
+    val size = ButtonDefaults.MediumContainerHeight
+    ElevatedButton(
+        modifier = Modifier.heightIn(size),
+        colors = ButtonDefaults.elevatedButtonColors(
+            containerColor = MaterialTheme.colorScheme.background,
+            contentColor = MaterialTheme.colorScheme.primary
+        ),
+        contentPadding = ButtonDefaults.contentPaddingFor(size),
+        elevation = ButtonDefaults.elevatedButtonElevation(8.dp),
+        onClick = onTap
+    ) {
+        Row {
+            Icon(
+                imageVector = icon,
+                contentDescription = "Medium Icon ElevatedButton"
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Text(text = text, style = ButtonDefaults.textStyleFor(size))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun MediumIconElevatedButtonPreview() {
+    MentalRecordAppTheme {
+        MediumIconElevatedButton(
+            icon = Icons.Default.Add,
+            text = "Button",
+            onTap = {}
+        )
+    }
+}
---
+++ app/src/main/java/jp/example/mentalrecordapplication/ui/recordlist/components/EmptyFilterRecordView.kt
@@ -0,0 +1,61 @@
+package jp.example.mentalrecordapplication.ui.recordlist.components
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FilterListOff
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import jp.example.mentalrecordapplication.R
+import jp.example.mentalrecordapplication.ui.common.button.MediumIconElevatedButton
+import jp.example.mentalrecordapplication.ui.theme.MentalRecordAppTheme
+
+@Composable
+fun EmptyFilterRecordView(
+    onResetFilterButtonTap: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 24.dp, vertical = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(id = R.string.empty_filter_record_msg),
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.secondary
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        MediumIconElevatedButton(
+            icon = Icons.Default.FilterListOff,
+            text = stringResource(id = R.string.reset_filter),
+            onTap = onResetFilterButtonTap
+        )
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun EmptyFilterRecordViewPreview() {
+    MentalRecordAppTheme {
+        Surface {
+            EmptyFilterRecordView(onResetFilterButtonTap = {})
+        }
+    }
+}
