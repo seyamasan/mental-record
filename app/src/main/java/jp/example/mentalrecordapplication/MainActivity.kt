2c06bdbf30aacdf726dfaa285d8b08811aeae9ef
seyamasan <kst_apr_24@icloud.com>
2025-09-02 19:31:45 +0900

    feat: Implemented onRequestPermissionsResult handling

--- app/src/main/java/jp/example/mentalrecordapplication/MainActivity.kt
+++ app/src/main/java/jp/example/mentalrecordapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package jp.example.mentalrecordapplication
 
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.compose.setContent
@@ -11,8 +12,12 @@ import dagger.hilt.android.AndroidEntryPoint
 import jp.example.mentalrecordapplication.navigator.AppNavigatorImpl
 import jp.example.mentalrecordapplication.ui.theme.MentalRecordAppTheme
 
+private const val REQUEST_RECORD_AUDIO_PERMISSION = 200
+
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private var permissionToRecordAccepted = false
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +30,22 @@ class MainActivity : AppCompatActivity() {
                 AppNavigatorImpl(navController).NavigateTo()
             }
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        permissionToRecordAccepted = if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
+            grantResults[0] == PackageManager.PERMISSION_GRANTED
+        } else {
+            false
+        }
+
+        if (!permissionToRecordAccepted) finish() // 録音の権限が付与されていなければActivityを終了
     }
 }
 
