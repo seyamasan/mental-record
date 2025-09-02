1de5d8146c192f02a0954306bebace5c92170d3a
seyamasan <kst_apr_24@icloud.com>
2025-09-02 20:13:17 +0900

    feat: Created AudioRecorderUtil

---
+++ app/src/main/java/jp/example/mentalrecordapplication/utils/AudioRecorderUtil.kt
@@ -0,0 +1,63 @@
+package jp.example.mentalrecordapplication.utils
+
+import android.content.Context
+import android.media.MediaRecorder
+import android.os.Build
+import android.util.Log
+import java.io.File
+import java.io.IOException
+
+class AudioRecorderUtil() {
+
+    private var mediaRecorder: MediaRecorder? = null
+
+    // 録音を開始
+    fun startRecording(
+        context: Context,
+        fileName: String = "test.3gp"
+    ): Boolean {
+        val filePath = File(context.filesDir, fileName).absolutePath
+
+        mediaRecorder = createMediaRecorder(context).apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setOutputFile(filePath)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+
+            try {
+                prepare()
+            } catch (e: IOException) {
+                Log.e(TAG, "prepare() failed")
+                return false
+            }
+
+            start()
+        }
+
+        return true
+    }
+
+    // 録音を停止
+    fun stopRecording() {
+        mediaRecorder?.apply {
+            stop()
+            release()
+        }
+
+        mediaRecorder = null
+    }
+
+    private fun createMediaRecorder(context: Context): MediaRecorder {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            // API 31以上は新コンストラクタ
+            MediaRecorder(context)
+        } else {
+            // API 30以下は旧コンストラクタ（非推奨）
+            MediaRecorder()
+        }
+    }
+
+    companion object {
+        private const val TAG = "AudioRecorderUtil"
+    }
+}
