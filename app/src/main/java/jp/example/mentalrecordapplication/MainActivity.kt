bf9dc63516d5388df2f51ae95779b028560a85ad
seyamasan <kst_apr_24@icloud.com>
2025-09-09 19:53:04 +0900

    fix: 録音の権限をリクエストできるように修正

--- app/src/main/java/jp/example/mentalrecordapplication/MainActivity.kt
+++ app/src/main/java/jp/example/mentalrecordapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package jp.example.mentalrecordapplication
 
+import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
@@ -7,21 +8,22 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import jp.example.mentalrecordapplication.navigator.AppNavigatorImpl
 import jp.example.mentalrecordapplication.ui.theme.MentalRecordAppTheme
 
-private const val REQUEST_RECORD_AUDIO_PERMISSION = 200
-
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private var permissionToRecordAccepted = false
+    private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION)
 
         enableEdgeToEdge()
         setContent {
@@ -39,13 +41,17 @@ class MainActivity : AppCompatActivity() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        permissionToRecordAccepted = if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
+        val permissionToRecordAccepted = if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
             grantResults[0] == PackageManager.PERMISSION_GRANTED
         } else {
             false
         }
 
         if (!permissionToRecordAccepted) finish() // 録音の権限が付与されていなければActivityを終了
+    }
+
+    companion object {
+        const val REQUEST_RECORD_AUDIO_PERMISSION = 200
     }
 }
 
