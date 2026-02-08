902fb7b0333f1f824a452f9bfb88c66223ae9052
seyamasan <kst_apr_24@icloud.com>
2026-02-08 14:56:04 +0900

    add: エポックミリビョウから時間帯を取得する処理を追加

--- app/src/main/java/jp/example/mentalrecordapplication/utils/DateUtil.kt
+++ app/src/main/java/jp/example/mentalrecordapplication/utils/DateUtil.kt
@@ -2,8 +2,11 @@ package jp.example.mentalrecordapplication.utils
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import jp.example.mentalrecordapplication.utils.types.TimeOfDayType
 import java.text.SimpleDateFormat
+import java.time.Instant
 import java.time.LocalDate
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.util.Date
 import java.util.Locale
@@ -24,5 +27,22 @@ object DateUtil {
     fun getStringNow(): String {
         val millis = System.currentTimeMillis()
         return convertMillisToDate(millis)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun convertMillisToTimeOfDay(millis: Long): TimeOfDayType {
+        val zoneId = ZoneId.systemDefault()
+
+        val localTime = Instant.ofEpochMilli(millis)
+            .atZone(zoneId)
+            .toLocalTime()
+
+        val hour = localTime.hour // 0〜23
+
+        return when (hour) {
+            in 5..10 -> TimeOfDayType.MORNING     // 5:00〜10:59
+            in 11..17 -> TimeOfDayType.NOON  // 11:00〜17:59
+            else -> TimeOfDayType.NIGHT           // それ以外
+        }
     }
 }
