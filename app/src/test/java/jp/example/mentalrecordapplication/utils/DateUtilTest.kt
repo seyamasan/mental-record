d078af70bae62416d0001a84fb15e99be3327474
seyamasan <kst_apr_24@icloud.com>
2026-02-08 14:56:38 +0900

    test: convertMillisToTimeOfDayのUnitテストを追加

--- app/src/test/java/jp/example/mentalrecordapplication/utils/DateUtilTest.kt
+++ app/src/test/java/jp/example/mentalrecordapplication/utils/DateUtilTest.kt
@@ -1,6 +1,7 @@
 package jp.example.mentalrecordapplication.utils
 
 import com.google.common.truth.Truth.assertThat
+import jp.example.mentalrecordapplication.utils.types.TimeOfDayType
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -26,8 +27,7 @@ class DateUtilTest {
     @Test
     fun convertMillisToDate_returns_formatted_date_string() {
         // Given
-        // Timestamp in milliseconds for 00:00:00 UTC on July 17, 2025 (2025年7月17日 00:00:00 UTC のミリ秒)
-        val millis = 1752710400000L
+        val millis = 1752710400000L // 2025年7月17日 09:00 Japan のミリ秒
         val formattedDateString = "2025/7/17"
 
         // When
@@ -47,5 +47,77 @@ class DateUtilTest {
 
         // Then
         assertThat(localDate).isEqualTo(LocalDate.of(2025, 7, 17))
+    }
+
+    @Test
+    fun convertMillisToTimeOfDay_returns_morning_at_morning_start() {
+        // Given
+        val millis = 1770494400000L // 2026年2月8日 05:00 Japan のミリ秒
+        val expect = TimeOfDayType.MORNING
+
+        // When
+        val timeOfDay = DateUtil.convertMillisToTimeOfDay(millis)
+
+        assertThat(timeOfDay).isEqualTo(expect)
+    }
+
+    @Test
+    fun convertMillisToTimeOfDay_returns_morning_at_morning_end() {
+        // Given
+        val millis = 1770515940000L // 2026年2月8日 10:59 Japan のミリ秒
+        val expect = TimeOfDayType.MORNING
+
+        // When
+        val timeOfDay = DateUtil.convertMillisToTimeOfDay(millis)
+
+        assertThat(timeOfDay).isEqualTo(expect)
+    }
+
+    @Test
+    fun convertMillisToTimeOfDay_returns_noon_at_noon_start() {
+        // Given
+        val millis = 1770516000000L // 2026年2月8日 11:00 Japan のミリ秒
+        val expect = TimeOfDayType.NOON
+
+        // When
+        val timeOfDay = DateUtil.convertMillisToTimeOfDay(millis)
+
+        assertThat(timeOfDay).isEqualTo(expect)
+    }
+
+    @Test
+    fun convertMillisToTimeOfDay_returns_noon_at_noon_end() {
+        // Given
+        val millis = 1770541140000L // 2026年2月8日 17:59 Japan のミリ秒
+        val expect = TimeOfDayType.NOON
+
+        // When
+        val timeOfDay = DateUtil.convertMillisToTimeOfDay(millis)
+
+        assertThat(timeOfDay).isEqualTo(expect)
+    }
+
+    @Test
+    fun convertMillisToTimeOfDay_returns_night_at_night_start() {
+        // Given
+        val millis = 1770541200000L // 2026年2月8日 18:00 Japan のミリ秒
+        val expect = TimeOfDayType.NIGHT
+
+        // When
+        val timeOfDay = DateUtil.convertMillisToTimeOfDay(millis)
+
+        assertThat(timeOfDay).isEqualTo(expect)
+    }
+
+    @Test
+    fun convertMillisToTimeOfDay_returns_night_at_night_end() {
+        // Given
+        val millis = 1770494340000L // 2026年2月8日 04:59 Japan のミリ秒
+        val expect = TimeOfDayType.NIGHT
+
+        // When
+        val timeOfDay = DateUtil.convertMillisToTimeOfDay(millis)
+
+        assertThat(timeOfDay).isEqualTo(expect)
     }
 }
