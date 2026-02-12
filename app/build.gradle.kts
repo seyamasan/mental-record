e8e9e32624e97110c9b8af4a88722e881814a528
seyamasan <kst_apr_24@icloud.com>
2026-02-12 21:24:33 +0900

    upgrade: バージョンアップ

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "jp.example.mentalrecordapplication"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.0.2"
+        versionCode = 11
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
