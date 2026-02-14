ff85db657e349ff6c1c8ed33f85e8fc6d310438b
seyamasan <kst_apr_24@icloud.com>
2026-02-14 11:31:32 +0900

    chore: グラフを作るためにvicoを追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -103,6 +103,9 @@ dependencies {
     implementation(libs.androidx.hilt.navigation.compose)
     ksp(libs.hilt.compiler)
 
+    // vico
+    implementation(libs.vico.compose)
+
     // test
     testImplementation(libs.junit)
     testImplementation(libs.truth)
